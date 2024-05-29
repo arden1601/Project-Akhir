9e61b4c7bfeef074739c29f276fd66f095154e2a
arden1601 <corneliusardensatwikahermawan@mail.ugm.ac.id>
2024-05-29 19:06:41 +0700

    feat: add dummy customer

---
+++ Task1/add_dummy_customer.sql
@@ -0,0 +1,22 @@
+INSERT INTO customer (customer_name, street, city, customer_state, country, telephone)
+VALUES
+('Budi Santoso', 'Jalan Malioboro 123', 'Yogyakarta', 'DI Yogyakarta', 'Indonesia', '0274-555-1234'),
+('Siti Aminah', 'Jalan Solo 45', 'Yogyakarta', 'DI Yogyakarta', 'Indonesia', '0274-555-2345'),
+('Ahmad Wijaya', 'Jalan Parangtritis 67', 'Yogyakarta', 'DI Yogyakarta', 'Indonesia', '0274-555-3456'),
+('Rini Suryani', 'Jalan Magelang 89', 'Yogyakarta', 'DI Yogyakarta', 'Indonesia', '0274-555-4567'),
+('Dewi Lestari', 'Jalan Kaliurang KM 10', 'Yogyakarta', 'DI Yogyakarta', 'Indonesia', '0274-555-5678'),
+('Andi Prasetyo', 'Jalan Diponegoro 101', 'Jakarta', 'DKI Jakarta', 'Indonesia', '021-555-1234'),
+('Nurul Hidayati', 'Jalan Sudirman 12', 'Bandung', 'Jawa Barat', 'Indonesia', '022-555-2345'),
+('Teguh Purnomo', 'Jalan Gajah Mada 34', 'Surabaya', 'Jawa Timur', 'Indonesia', '031-555-3456'),
+('Lina Marlina', 'Jalan Merdeka 56', 'Medan', 'Sumatera Utara', 'Indonesia', '061-555-4567'),
+('Heri Kurniawan', 'Jalan Diponegoro 78', 'Semarang', 'Jawa Tengah', 'Indonesia', '024-555-5678'),
+('Sri Wahyuni', 'Jalan Ahmad Yani 90', 'Makassar', 'Sulawesi Selatan', 'Indonesia', '0411-555-6789'),
+('Agus Setiawan', 'Jalan Thamrin 101', 'Palembang', 'Sumatera Selatan', 'Indonesia', '0711-555-7890'),
+('Rina Suryani', 'Jalan Pemuda 12', 'Malang', 'Jawa Timur', 'Indonesia', '0341-555-8901'),
+('Bayu Nugroho', 'Jalan Diponegoro 34', 'Denpasar', 'Bali', 'Indonesia', '0361-555-9012'),
+('Mega Utami', 'Jalan Sudirman 56', 'Balikpapan', 'Kalimantan Timur', 'Indonesia', '0542-555-1234'),
+('Rudi Hartono', 'Jalan Merdeka 78', 'Pekanbaru', 'Riau', 'Indonesia', '0761-555-2345'),
+('Yusuf Maulana', 'Jalan Sisingamangaraja 90', 'Pontianak', 'Kalimantan Barat', 'Indonesia', '0561-555-3456'),
+('Mira Anindya', 'Jalan Dr. Sutomo 12', 'Manado', 'Sulawesi Utara', 'Indonesia', '0431-555-4567'),
+('Wawan Hermawan', 'Jalan Pattimura 34', 'Banjarmasin', 'Kalimantan Selatan', 'Indonesia', '0511-555-5678'),
+('Putri Ayu', 'Jalan Veteran 56', 'Padang', 'Sumatera Barat', 'Indonesia', '0751-555-6789');
